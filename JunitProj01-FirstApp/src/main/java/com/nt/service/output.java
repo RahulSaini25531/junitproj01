cdc02c21e33a1d002ab69aef7f6f9f589dcd3f18
rahul saini <rahulsaini3501@gmail.com>
2024-02-27 16:32:21 +0530

    task

--- JunitProj01-FirstApp/src/main/java/com/nt/service/output.java
+++ JunitProj01-FirstApp/src/main/java/com/nt/service/output.java
@@ -5,6 +5,7 @@ public class output {
 	public static void main(String[] args) {
 		
 		subOPeration sub1 = new subOPeration();
+		System.out.println("----------------------");
 		System.out.println("sub :"+sub1.sub(12, 8));
 	}
 
