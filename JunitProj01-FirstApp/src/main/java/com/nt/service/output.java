d0971869cbeeaaba3e8c4f49e6ce95ce6bfa0b90
rahul saini <rahulsaini3501@gmail.com>
2024-02-27 16:27:23 +0530

    task1

---
+++ JunitProj01-FirstApp/src/main/java/com/nt/service/output.java
@@ -0,0 +1,11 @@
+package com.nt.service;
+
+public class output {
+	
+	public static void main(String[] args) {
+		
+		subOPeration sub1 = new subOPeration();
+		System.out.println("sub :"+sub1.sub(12, 8));
+	}
+
+}
---
+++ JunitProj01-FirstApp/src/main/java/com/nt/service/subOPeration.java
@@ -0,0 +1,10 @@
+package com.nt.service;
+
+public class subOPeration {
+	
+	public int sub(int x, int y)
+	{
+		return x-y;
+	}
+
+}
